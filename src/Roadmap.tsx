79f7c15fc969bf9acb799cf7b63cc1118443d80f
unknown <dmsrhkdakdmf@naver.com>
2023-02-01 16:27:24 +0900

    add roadmap

--- src/Roadmap.tsx
+++ src/Roadmap.tsx
@@ -51,7 +51,7 @@ function Roadmap(){
 
       const scrollToTop = () => {
           window.scroll({
-              top: 500,
+              top: 700,
               behavior: 'smooth'
           })
 
@@ -68,10 +68,30 @@ function Roadmap(){
 
     function Roadmap (){
       
+      
       return(
         <>
-    
- 
+          <div className="storytitleContainer">
+            <h1 className="gradient-text">Solove Story : Solove World!</h1>
+            <p className="stroyfirsttext">In 2050, I discovered Solove World while traveling in a spacecraft
+              </p>
+              <p className="stroyfirsttext">made by Elon Musk.
+              </p>
+            <div className="storyfirst">  
+            </div>
+              <p className="stroyfirsttext">Solove World has various love.
+              </p>
+              <p className="stroyfirsttext">And everyone loved the various love.
+              </p>
+            <div className="storyfirst"> 
+            </div>
+            <p className="stroyfirsttext">Seeing advanced love, Human settled in Solove World.
+              </p>
+              <p className="stroyfirsttext">Come to Solove World, too.
+              </p>
+            <div className="storyfirst">  
+            </div>
+          </div> 
         </>  
       )
     }
