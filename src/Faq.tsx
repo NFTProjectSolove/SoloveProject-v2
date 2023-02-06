3f0c3c380f3ca716a0e29e4d93d1bf846d4b4a39
unknown <dmsrhkdakdmf@naver.com>
2023-02-06 18:16:20 +0900

    roadmap modify

--- src/Faq.tsx
+++ src/Faq.tsx
@@ -6,15 +6,15 @@ function Faq() {
         <div className="tranPage">
         <div id='faq' style={{ backgroundColor: 'black', width: '100vw', height: '150vh', position: 'relative', top: '0', overflow: 'hidden' }}>
             <div style={{ width: '100%', position: 'absolute', top: '15%', margin: '0 auto', display: 'flex', flexDirection: 'column' }}>
-            <div className="FAQtext">FAQ</div>
-            <Accordian />
+            <div className="FAQtext">F A Q</div>
+            <Accordian questiontext = "Hi" answertext = "bye"/>
           <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
+            <Accordian questiontext = "Hi" answertext = "bye"/>
           <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
+            <Accordian questiontext = "Hi" answertext = "bye"/>
           <div style={{paddingTop:'30px'}}></div>
-          <Accordian />
-            </div>
+            <Accordian questiontext = "Hi" answertext = "bye"/>
+        </div>
         </div>
         </div>
     )
