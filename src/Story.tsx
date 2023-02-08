43c9b313dc5cf6e4acc88c84b55856fa21448bef
ongsiru <gusdnr3998@naver.com>
2023-02-08 17:47:46 +0900

    ongsiru

--- src/Story.tsx
+++ src/Story.tsx
@@ -1,39 +1,26 @@
+import React, { useState, useEffect } from "react";
+import Typingword from './typingText';
+
 function Story (){
+  const [scrollPosition, setScrollPosition] = useState(0);
+  
+  const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", updateScroll);
+    }, []);
       
     return(
-      <div className="tranPage">
-        <div className="storytitleContainer">
-          <h1 className="gradient-text">Solove Story : Solove World!</h1>
-          <p className="stroyfirsttext">In 2050, I discovered Solove World while traveling in a spacecraft
-            </p>
-            <p className="stroyfirsttext">made by Elon Musk.
-            </p>
-          <div className="storyfirst">  
-          </div>
-            <p className="stroyfirsttext">Solove World has various love.
-            </p>
-            <p className="stroyfirsttext">And everyone loved the various love.
-            </p>
-          <div className="storyfirst"> 
-          </div>
-          <p className="stroyfirsttext">Seeing advanced love, Human settled in Solove World.
-            </p>
-            <p className="stroyfirsttext">Come to Solove World, too.
-            </p>
-          <div className="storyfirst">  
+      <>
+        <div className="storyContainer">
+          <div className="storySection1">
+            {scrollPosition >= 150 ? <Typingword typingText="안녕하세요. 천재 최현욱입니다."></Typingword> : null}
           </div>
         </div> 
-      </div>  
+      </>  
     )
   }
-
-function About(){
-
-    return(
-        <div className="tranPage">
-            <Story></Story>
-        </div>
-    )
-}
 
 export default Story
