d89c2b64ec00a0a4fef888dc9e026c8feafdb481
ongsiru <gusdnr3998@naver.com>
2023-02-08 12:54:55 +0900

    ongsiru

--- src/Faq.tsx
+++ src/Faq.tsx
@@ -7,13 +7,13 @@ function Faq() {
         <div id='faq' style={{ backgroundColor: 'black', width: '100vw', height: '150vh', position: 'relative', top: '0', overflow: 'hidden' }}>
             <div style={{ width: '100%', position: 'absolute', top: '15%', margin: '0 auto', display: 'flex', flexDirection: 'column' }}>
             <div className="FAQtext">F A Q</div>
-            <Accordian questiontext = "Hi" answertext = "bye"/>
+            <Accordian questiontext = "What sort of stuff do you do on the weekends?" answertext = "I work as a tour guide for a local tour company. I’ve worked there for three years now."/>
           <div style={{paddingTop:'30px'}}></div>
-            <Accordian questiontext = "Hi" answertext = "bye"/>
+            <Accordian questiontext = "Do you feel like grabbing a bite?" answertext = "I work as a tour guide for a local tour company. I’ve worked there for three years now."/>
           <div style={{paddingTop:'30px'}}></div>
-            <Accordian questiontext = "Hi" answertext = "bye"/>
+            <Accordian questiontext = "have you heard that I moved to a new apartment?" answertext = "I work as a tour guide for a local tour company. I’ve worked there for three years now."/>
           <div style={{paddingTop:'30px'}}></div>
-            <Accordian questiontext = "Hi" answertext = "bye"/>
+            <Accordian questiontext = "Would you be up for a movie some time?" answertext = "I work as a tour guide for a local tour company. I’ve worked there for three years now."/>
         </div>
         </div>
         </div>
