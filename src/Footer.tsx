dd175db30906778943d11963e47fe022826ae082
ongsiru <gusdnr3998@gmail.com>
2023-02-10 02:09:07 +0900

    ongsiru

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,7 @@ function Footer(){
 
     return(
         <div className="footerContainer">
-            ⓒSoloveNFT, Inc. 2023 | <a href="mailto:just5538@gmail.com" > Click to Mail</a>
+            ⓒSoloveNFT, Inc. 2023&nbsp;|&nbsp;<a href="mailto:just5538@gmail.com" >Click to Mail</a>
         </div>
     )
 }
