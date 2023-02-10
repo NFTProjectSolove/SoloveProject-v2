deee72aca3052be91a7c43c48db0dc8c4aadf664
StrongTyphoon <just5538@gmail.com>
2023-02-10 13:56:12 +0900

    Add SnsLogo Animation1

--- src/SnsLogo.tsx
+++ src/SnsLogo.tsx
@@ -1,13 +1,43 @@
+import {BsPlusCircleFill,BsFillXCircleFill } from 'react-icons/bs'
+import React, {useEffect, useState, useRef} from 'react';
 
 function SnsLogo(){
+    const [buttonState, SetButtonState] = useState<boolean>(false);     //false 시 close 된 상태 true 시 버튼이 나타나는 상태
+    const openUl = useRef<HTMLUListElement>(null);
+    const closeUl = useRef<HTMLUListElement>(null);
+
+    useEffect(()=>{
+        if(buttonState){
+            if(openUl.current && closeUl.current){
+                openUl.current.style.display='block'
+                closeUl.current.style.display='none'
+                openUl.current.classList.add("snsAppear")
+            }
+        }
+        else{
+            if(openUl.current && closeUl.current){
+                openUl.current.style.display='none'
+                closeUl.current.style.display='block'
+            } 
+        }  
+            
+    },[buttonState])
 
     return(
         <div className="snsLogoContainer">
-            <li>
-                <img src="/opensea.svg" alt='openseaLogo' />
-                <img src="/twitter.svg" alt='twitterLogo' /> 
-                <img src="/discord.svg" alt='discordLogo' /> 
-            </li>
+            <div style={{position:'relative', width:'auto'}}>
+                <ul className="openUl" ref={openUl} >
+                    <li><img src="/opensea.svg" alt='openseaLogo' /></li>
+                    <li><img src="/twitter.svg" alt='twitterLogo' /> </li>
+                    <li><img src="/discord.svg" alt='discordLogo' /> </li>
+                    <li><div className="snsButton snsCloseButton" onClick={()=>SetButtonState(false)}><BsFillXCircleFill /></div></li>
+                </ul>
+
+                <ul className="closeUl" ref={closeUl}>
+                    <li><div className="snsButton snsOpenButton" onClick={()=>SetButtonState(true)}><BsPlusCircleFill /></div></li>
+                </ul>
+                
+            </div>
         </div>
     )
 }
